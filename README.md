a44f2c7e605b5bfb5e696774de8d09c9ab8e55e6
Samir Lachgar <sam.lachgar@gmail.com>
2016-08-11 18:59:53 +0200

    Indent correction

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ What does it do ?
 Well it handles most of a Steam Trade Scenario and makes it easy for you to operate and handle trades.
 The code is pretty self explanatory but i will maybe write some code example and how to use it later.
 
+## Installation
+
+  npm install
+
 ## License
 
 Use it for anything and however you want. Licence IDC (I don't care)
--- trade-bot.js
+++ trade-bot.js
@@ -13,20 +13,20 @@ const queue = kue.createQueue();
 
 
 /**
- * You need to setup your API Endpoint if you want to get
- * Updates of your trades.
- */
+* You need to setup your API Endpoint if you want to get
+* Updates of your trades.
+*/
 const API_URL = 'http://localhost:3000'
 const API_UPDATE_STATUS = API_URL + '/api/trade/status'
 const API_UPDATE_TRADE_ID = API_URL + '/api/trade/tradeid'
 /**
- * Steam / Trade Manager
- */
+* Steam / Trade Manager
+*/
 const client = new SteamUser()
 const manager = new TradeOfferManager({
-	"steam": client, // Polling every 30 seconds is fine since we get notifications from Steam
-	"domain": "localhost.com", // Our domain is example.com
-	"language": "en", // We want English item descriptions
+  "steam": client, // Polling every 30 seconds is fine since we get notifications from Steam
+  "domain": "localhost.com", // Our domain is example.com
+  "language": "en", // We want English item descriptions
   "cancelTime" : "600000"
 })
 const TradeHelper = require('./lib/tradehelper')(manager);
@@ -34,9 +34,9 @@ const community = new Steamcommunity()
 
 // Steam logon options
 const logOnOptions = {
-	"accountName": "ACCOUNT_NAME",
-	"password": "PASSWORD",
-	"twoFactorCode": SteamTotp.getAuthCode("YOUR_CODE")
+  "accountName": "ACCOUNT_NAME",
+  "password": "PASSWORD",
+  "twoFactorCode": SteamTotp.getAuthCode("YOUR_CODE")
 }
 
 /// Bot Login and Setup Status
@@ -49,7 +49,7 @@ let isUpAndRunningObj = {
 }
 
 if (fs.existsSync('polldata.json')) {
-	manager.pollData = JSON.parse(fs.readFileSync('polldata.json'))
+  manager.pollData = JSON.parse(fs.readFileSync('polldata.json'))
 }
 
 
@@ -58,90 +58,90 @@ if (fs.existsSync('polldata.json')) {
 client.logOn(logOnOptions)
 
 client.on('loggedOn', function() {
-	console.log("Logged into Steam")
+  console.log("Logged into Steam")
 
 })
 
 client.on('webSession', function(sessionID, cookies) {
-	manager.setCookies(cookies, function(err) {
-		if (err) {
-			console.log(err)
-			process.exit(1) // Fatal error since we couldn't get our API key
-			return
-		}
-		console.log("Got API key: " + manager.apiKey)
+  manager.setCookies(cookies, function(err) {
+    if (err) {
+      console.log(err)
+      process.exit(1) // Fatal error since we couldn't get our API key
+      return
+    }
+    console.log("Got API key: " + manager.apiKey)
     isUpAndRunningObj.logged_in = true
-	})
+  })
 
-	community.setCookies(cookies)
-	community.startConfirmationChecker(30000, "identitySecret") // Checks and accepts confirmations every 30 seconds
+  community.setCookies(cookies)
+  community.startConfirmationChecker(30000, "identitySecret") // Checks and accepts confirmations every 30 seconds
 
   isUpAndRunningObj.cookies_set = true
 
 })
 
 manager.on('pollData', function(pollData) {
-	fs.writeFile('polldata.json', JSON.stringify(pollData));
+  fs.writeFile('polldata.json', JSON.stringify(pollData));
 });
 
 manager.on('newOffer', function(offer) {
-	console.log("New offer #" + offer.id + " from " + offer.partner.getSteam3RenderedID())
+  console.log("New offer #" + offer.id + " from " + offer.partner.getSteam3RenderedID())
   if(!offer.isOurOffer) {
     offer.decline(function(err) {
-  		if (err) {
-  			console.log("Unable to decline offer: ")
-  		} else {
-  			console.log("Offer declined")
-  		}
-  	})
+      if (err) {
+        console.log("Unable to decline offer: ")
+      } else {
+        console.log("Offer declined")
+      }
+    })
   }
 
 })
 
 manager.on('sentOfferChanged', function (offer, oldState) {
-    // Alert us when one of our offers is accepted
-    //
-    //
+  // Alert us when one of our offers is accepted
+  //
+  //
 
-    let tradeStatus = {
-      steam_trade_id : offer.id,
-      trade_status : ''
-    }
+  let tradeStatus = {
+    steam_trade_id : offer.id,
+    trade_status : ''
+  }
 
-    if (offer.state == TradeOfferManager.ETradeOfferState.Accepted) {
-      tradeStatus.trade_status = 'accepted'
-      console.log("Our sent offer #"+ offer.id + " has been accepted.");
+  if (offer.state == TradeOfferManager.ETradeOfferState.Accepted) {
+    tradeStatus.trade_status = 'accepted'
+    console.log("Our sent offer #"+ offer.id + " has been accepted.");
 
-      offer.getReceivedItems(function(err, items){
-        console.log(items)
+    offer.getReceivedItems(function(err, items){
+      console.log(items)
+    })
+    updateTradeStatus(tradeStatus).catch((error) => {
+      console.log(error)
+    })
+  }else if (offer.state == TradeOfferManager.ETradeOfferState.Declined) {
+    tradeStatus.trade_status = 'declined'
+
+    console.log("Our sent offer #"+ offer.id + " has been declined.");
+
+    updateTradeStatus(tradeStatus).catch((error) => {
+      console.log(error)
+    })
+  } else {
+    tradeStatus.trade_status = 'cancelled'
+    updateTradeStatus(tradeStatus).then(() => {
+
+      console.log('Offer cancelled update '+ offer.id )
+      offer.cancel(function(err){
+        if(!err){
+          console.log('Offer cancelled successfully')
+        }else {
+          console.log(err)
+        }
       })
-      updateTradeStatus(tradeStatus).catch((error) => {
-        console.log(error)
-      })
-    }else if (offer.state == TradeOfferManager.ETradeOfferState.Declined) {
-      tradeStatus.trade_status = 'declined'
-
-      console.log("Our sent offer #"+ offer.id + " has been declined.");
-
-      updateTradeStatus(tradeStatus).catch((error) => {
-        console.log(error)
-      })
-    } else {
-      tradeStatus.trade_status = 'cancelled'
-      updateTradeStatus(tradeStatus).then(() => {
-
-        console.log('Offer cancelled update '+ offer.id )
-        offer.cancel(function(err){
-          if(!err){
-            console.log('Offer cancelled successfully')
-          }else {
-            console.log(err)
-          }
-        })
-      }).catch((error) => {
-        console.log(error)
-      })
-    }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
 
 });
 
@@ -183,14 +183,14 @@ manager.on('sentPendingOfferCanceled', function(offer) {
 })
 
 /**
- * You need to push jobs to this queue
- * Verication_code is your own generated code
- * Trade_uid is your trade id in the database
- * steam_id the user's steam id you are trading with
- * User_items an array of items (steam-tradeoffer-manager format)
- * Bot_items same as above but your items
- * UserToken, the user's steam token (If you are not friends on steam)
- */
+* You need to push jobs to this queue
+* Verication_code is your own generated code
+* Trade_uid is your trade id in the database
+* steam_id the user's steam id you are trading with
+* User_items an array of items (steam-tradeoffer-manager format)
+* Bot_items same as above but your items
+* UserToken, the user's steam token (If you are not friends on steam)
+*/
 queue.process('trade', function(job, done){
   const data = job.data
   const verificationCode = data.verification_code
@@ -212,8 +212,8 @@ queue.process('trade', function(job, done){
 });
 
 /**
- * Create a new Trade Offer with the items to Give/Receive
- */
+* Create a new Trade Offer with the items to Give/Receive
+*/
 const createOffer = (data) => {
   const verificationCode = data.verification_code
   const tradeDbUID = data.trade_uid
@@ -229,53 +229,53 @@ const createOffer = (data) => {
   }
 
   TradeHelper.getEscrowDays(customerSteamID, userToken)
-    .then(TradeHelper.checkEscrowAcceptance)
-    .then(() => {
-      return TradeHelper.loadBotAndUserInventory(customerSteamID, itemsToReceiveArray, itemsToGiveArray)
+  .then(TradeHelper.checkEscrowAcceptance)
+  .then(() => {
+    return TradeHelper.loadBotAndUserInventory(customerSteamID, itemsToReceiveArray, itemsToGiveArray)
+  })
+  .then((inventoryArray) => {
+    console.log(inventoryArray)
+    const botInv = _.filter(inventoryArray, { inventory_type : 'bot' })
+    const userInv = _.filter(inventoryArray, { inventory_type : 'user' })
+    console.log(userInv[0])
+    return Promise.resolve({
+      verification_code : verificationCode,
+      steam_user_id : customerSteamID,
+      bot_inventory : botInv[0].inventory,
+      user_inventory : userInv[0].inventory,
+      user_token : userToken
     })
-    .then((inventoryArray) => {
-      console.log(inventoryArray)
-      const botInv = _.filter(inventoryArray, { inventory_type : 'bot' })
-      const userInv = _.filter(inventoryArray, { inventory_type : 'user' })
-      console.log(userInv[0])
-      return Promise.resolve({
-        verification_code : verificationCode,
-        steam_user_id : customerSteamID,
-        bot_inventory : botInv[0].inventory,
-        user_inventory : userInv[0].inventory,
-        user_token : userToken
-      })
+  })
+  .then(TradeHelper.initiateTrade)
+  .then((tradeInfo) => {
+    const steamTradeId = tradeInfo.trade.id
+    const tradeStatus = tradeInfo.status
+    const tradeObj = {
+      steam_trade_id : steamTradeId,
+      trade_status : tradeStatus,
+      trade_db_uid : tradeDbUID
+    }
+    updateTradeID(tradeObj).then(() => {
+      return updateTradeStatus(tradeObj)
+    }).then(() => {
+      return done();
+    }).catch((err) => {
+      console.log(err)
     })
-    .then(TradeHelper.initiateTrade)
-    .then((tradeInfo) => {
-      const steamTradeId = tradeInfo.trade.id
-      const tradeStatus = tradeInfo.status
-      const tradeObj = {
-        steam_trade_id : steamTradeId,
-        trade_status : tradeStatus,
-        trade_db_uid : tradeDbUID
-      }
-      updateTradeID(tradeObj).then(() => {
-        return updateTradeStatus(tradeObj)
-      }).then(() => {
-        return done();
-      }).catch((err) => {
-        console.log(err)
-      })
-    })
-    .catch((error) => {
-      console.log('Error issued', error.message)
-      let tradeStatus = {
-        verification_code : verificationCode,
-        trade_status : 'error',
-        message : error.message
-      }
+  })
+  .catch((error) => {
+    console.log('Error issued', error.message)
+    let tradeStatus = {
+      verification_code : verificationCode,
+      trade_status : 'error',
+      message : error.message
+    }
 
-      updateTradeStatus(tradeStatus).catch((error) => {
-        console.log(error)
-      })
-      return done(new Error(error));
+    updateTradeStatus(tradeStatus).catch((error) => {
+      console.log(error)
     })
+    return done(new Error(error));
+  })
 }
 
 const updateTradeStatus = (data) => {
